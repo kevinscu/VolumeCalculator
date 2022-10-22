2d27308470984d138ccef6859a37bcb977a121a6
kevinscu <kevscullion@live.co.uk>
2022-10-22 23:01:39 +0100

    All junit tests added and working

--- src/test/java/ShapeTests.java
+++ src/test/java/ShapeTests.java
@@ -1,32 +1,50 @@
 import org.junit.Assert;
-import org.junit.Before;
 import org.junit.Test;
 
 
 public class ShapeTests {
 
-    Cube cube;
-    //RegularTetrahedron regularTetrahedron;
-    //Sphere sphere;
-
-    @Before
-    public void setUpTests() {
-        cube = new Cube(1);
+    Cube cubeTest;
+    Sphere sphereTest;
+    RegularTetrahedron RegularTetrahedronTest;
+    
+    @Test
+    public void testSphereVolumeWithRadiusOne()  {
+        sphereTest = new Sphere(1);
+        Assert.assertEquals(4.19, sphereTest.calcVolume(), 0.01);
     }
-
-
-
-
-
 
     @Test
-
-    public void testCubeVolume(){
-
-        Assert.assertEquals(1, cube.calcVolume(), 0.01);
+    public void testSphereVolumeWithRadiusFive()  {
+        sphereTest = new Sphere(5);
+        Assert.assertEquals(523.6, sphereTest.calcVolume(), 0.3);
     }
 
+    @Test
+    public void testCubeVolumeWithHeightOne() {
+        cubeTest = new Cube(1);
+        Assert.assertEquals(1, cubeTest.calcVolume(), 0.01);
+    }
 
+    @Test
+    public void testCubeVolumeWithHeightFive() {
+        cubeTest = new Cube(5);
+        Assert.assertEquals(125, cubeTest.calcVolume(), 0.01);
+    }
+
+    @Test
+    public void testRegularTetrahedronVolumeWithEdgeOne() {
+        RegularTetrahedronTest = new RegularTetrahedron(1);
+        Assert.assertEquals(0.12, RegularTetrahedronTest.calcVolume(), 0.01);
+    }
+
+    @Test
+    public void testRegularTetrahedronVolumeWithEdgeFive() {
+        RegularTetrahedronTest = new RegularTetrahedron(5);
+        Assert.assertEquals(14.73, RegularTetrahedronTest.calcVolume(), 0.01);
+    }
+
+    //End Of Tests
 }
 
 
